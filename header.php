7362b2414bd442a1e441f3cedd7687fa3fa8195c
anizcse <129384204+anizcse@users.noreply.github.com>
2023-04-20 17:38:20 +0530

    add inside head tag
    Add this code inside head tag.

---
+++ header.php
@@ -0,0 +1,9 @@
+<meta property="og:title" content="<?php echo get_the_title();?>">
+<meta property="og:type" content="website">
+<meta property="og:url" content="<?php echo get_the_permalink();?>">
+<meta property="og:description" content="<?php echo the_excerpt();?>">
+
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:image:secure_url" content="<?php echo get_the_post_thumbnail_url();?>">
+<link rel="image_src" href="<?php echo get_the_post_thumbnail_url(); ?>">
